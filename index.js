9d41e9433636f0c3b7c86276f80ccd98da3952e4
alirezamou <am808036@gmail.com>
2023-03-26 12:27:16 +0330

    move the routes into a json file name 'routes.json', read and setup routes from there. we can add or remove routes from 'routes.json' file

--- index.js
+++ index.js
@@ -1,87 +1,50 @@
+const fs = require("fs");
+
 const express = require("express");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
 const { HttpsProxyAgent } = require("https-proxy-agent");
 
+// TOR http proxy exposed on port = 8118 on localhost
 const agent = new HttpsProxyAgent("http://127.0.0.1:8118");
 
 const app = express();
 
 const PORT = 3002;
 const HOST = "localhost";
-const API_SERVICE_JSONPLACEHOLDER = "https://jsonplaceholder.typicode.com";
-const API_SERVICE_PIXABAY = "https://pixabay.com/api";
-const API_SERVICE_UNSPLASH = "https://source.unsplash.com/random";
-const API_SERVICE_TVMAZE = "https://api.tvmaze.com";
-const API_SERVICE_TVMAZE_STATIC = "https://static.tvmaze.com";
 
 app.use(cors());
 
 app.use(morgan("dev"));
 
-/*
-app.use(
-  "/json_placeholder",
-  createProxyMiddleware({
-    target: API_SERVICE_JSONPLACEHOLDER,
-    changeOrigin: true,
-    agent,
-    pathRewrite: {
-      [`^/json_placeholder`]: "",
-    },
-  })
-);
+fs.readFile("routes.json", { encoding: "utf8" }, (error, data) => {
+  if (error) {
+    console.log(error);
+  }
 
-app.use(
-  "/unsplash",
-  createProxyMiddleware({
-    target: API_SERVICE_UNSPLASH,
-    changeOrigin: true,
-    agent,
-    pathRewrite: {
-      [`/unsplash`]: "",
-    },
-  })
-);
+  let routes;
+  routes = JSON.parse(data);
+  setupRoutes(routes);
 
-app.use(
-  "/pixabay",
-  createProxyMiddleware({
-    target: API_SERVICE_PIXABAY,
-    changeOrigin: true,
-    agent,
-    pathRewrite: {
-      [`/pixabay`]: "",
-    },
-  })
-);
-*/
-
-app.use(
-  "/tvmaze",
-  createProxyMiddleware({
-    target: API_SERVICE_TVMAZE,
-    changeOrigin: true,
-    agent,
-    pathRewrite: {
-      [`^/tvmaze`]: "",
-    },
-  })
-);
-
-app.use(
-  "/tvmaze_statics",
-  createProxyMiddleware({
-    target: API_SERVICE_TVMAZE_STATIC,
-    changeOrigin: true,
-    agent,
-    pathRewrite: {
-      [`^/tvmaze_statics`]: "",
-    },
-  })
-);
-
-app.listen(PORT, HOST, () => {
-  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+  });
 });
+
+function setupRoutes(routes) {
+  Object.keys(routes).forEach((routeKey) => {
+    console.log(routeKey);
+    app.use(
+      routeKey,
+      createProxyMiddleware({
+        target: routes[routeKey].api_url,
+        changeOrigin: true,
+        agent,
+        pathRewrite: {
+          [`${routeKey}`]: "",
+        },
+      })
+    );
+  });
+}
